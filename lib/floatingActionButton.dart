ba8436bad2fe7c3953763ce3c63771004ead91ab
asadpro <asadjani202@gmail.com>
2022-02-03 13:54:00 +0500

    adding recipes folder

---
+++ lib/floatingActionButton.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class clipOval extends StatelessWidget {
+  const clipOval({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('clipOval Screen'),
+      ),
+      body: Container(
+        color: Colors.green,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,74 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:play_with_widgets_part_1/clipOval.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+    routes: {
+      'route1': (context) => clipOvalWidget(),
+      'route2': (context) => clipOvalWidget(),
+    },
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  List<Widget> emptyListForBtn = [];
-
-  List<Widget> creatingButtons() {
-    for (int i = 0; i <= 10; i++) {
-      Widget addingToList = SizedBox.expand(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: Text(
-            'Widget ',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 34.0,
-            ),
-          ),
-        ),
-      );
-      emptyListForBtn.add(addingToList);
-    }
-    return emptyListForBtn;
-  }
+  MyApp({Key? key}) : super(key: key);
+  final List<Widget> emptyListForBtn = [];
+  List<String> nameOfWidget = [
+    'ClipRRect()',
+    'ClipOval()',
+    'CircleAvatar()',
+    'FadeInImage().assetNetwork()',
+    'AnimatedContainer()',
+    'FittedBox()',
+    'Flexible() ',
+    'FloatingActionButton()',
+    'BottomAppBar()',
+    'CircleBorder()'
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(),
-          body: ListWheelScrollView(
-            itemExtent: 130,
-            children: creatingButtons(),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'List of Widgets',
+            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: ListView(
+          // padding: EdgeInsets.all(15.0),
+          itemExtent: 87,
+          children: [
+            WidgetButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WidgetButton extends StatelessWidget {
+  // WidgetButton({required this.widgetName, required this.onPress});
+  // String widgetName;
+  // VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, 'route1');
+        },
+        child: Text(
+          'ClipRRect',
+          style: TextStyle(
+            fontSize: 35.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
